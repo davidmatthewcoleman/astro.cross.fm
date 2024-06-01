14898cad9e922c4899827d62b33db89c46db1426
davidmc.io <hi@davidmc.io>
2024-05-31 21:00:28 -0500

    Init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: vercel(),
+  adapter: node(),
   experimental: {
     contentCollectionCache: true,
   },
