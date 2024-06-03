ab2be754433a92b32a3b6e93db6901bee90bb85b
davidmc.io <hi@davidmc.io>
2024-06-03 04:20:48 -0500

    Init

---
+++ src/components/works/Masonry.jsx
@@ -0,0 +1,32 @@
+import Picture from "../../components/Picture.jsx";
+import React from "react";
+
+export default function Masonry({ data }) {
+    return (
+        <>
+            <link rel="stylesheet" href="/assets/css/app.css"/>
+            <div className="columns-1 sm:columns-2 md:columns-3 lg:columns-4 xl:columns-5" style={{ columnGap: `0.5rem` }}>
+                {data.map((work, index) => (
+                    <Picture
+                        key={index}
+                        alt={``}
+                        className="work__image mb-2 rounded-lg"
+                        source={
+                            {
+                                null: {
+                                    media: work,
+                                    params: {
+                                        'width': 1024,
+                                        'height': 1024,
+                                        'fit': 'clip'
+                                    }
+                                }
+                            }
+                        }
+                        style={{aspectRatio: `${work.mediaDetails.width}/${work.mediaDetails.height}`}}
+                    />
+                ))}
+            </div>
+        </>
+    );
+}
