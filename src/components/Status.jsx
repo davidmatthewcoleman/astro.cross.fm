e36517e603f8982625fb1df36fadbd6bee56c3c6
davidmc.io <hi@davidmc.io>
2024-11-15 16:47:02 -0600

    Init

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+import React from 'react';
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
 
 export default function Status() {
     const [discordData, setDiscordData] = useState(null);
@@ -63,12 +66,20 @@ export default function Status() {
     return (
         <>
             {show && !link && (
-                <span
-                    className="online-status"
-                    data-status={status}
+                <Tippy
+                    content={discordData.activities[0].state}
+                    arrow={false}
+                    placement="left"
+                    offset={[-1,3]}
+                    theme={'wolfhead'}
                 >
-                    {label}
-                </span>
+                    <span
+                        className="online-status"
+                        data-status={status}
+                    >
+                        {label}
+                    </span>
+                </Tippy>
             )}
             {show && link && (
                 <a
@@ -119,6 +130,127 @@ export default function Status() {
                 }
                 .online-status[data-status="live"] {
                     --color: #aa00ff;
+                }
+                
+                .tippy-box[data-theme~="wolfhead"] {
+                    font-size: 0.85rem;
+                    color: #bdbdbd;
+                    background-color: #252526;
+                    filter: drop-shadow(1px 1px 2px rgba(0, 0, 0, 0.25));
+                }
+                .tippy-box[data-theme~="wolfhead"] .tippy-content {
+                    padding: 0 0.5em;
+                    font-size: 0.8rem;
+                    line-height: 2;
+                }
+                .tippy-box[data-theme~="license"] .tippy-content {
+                    padding: 0.25em 0.5em;
+                    line-height: 1.5;
+                }
+                .tippy-box[data-theme~="wolfhead"] .tippy-arrow {
+                    transform: scale(0.75);
+                }
+                
+                .tippy-box[data-theme~="wolfhead"][data-placement~="top"] .tippy-arrow:before {
+                    bottom: -5px;
+                    border-top-color: #252526;
+                }
+                .tippy-box[data-theme~="wolfhead"][data-placement~="bottom"] .tippy-arrow:before {
+                    top: -5px;
+                    border-bottom-color: #252526;
+                }
+                .tippy-box[data-theme~="wolfhead"][data-placement~="left"] .tippy-arrow:before {
+                    right: -5px;
+                    border-left-color: #252526;
+                }
+                .tippy-box[data-theme~="wolfhead"][data-placement~="right"] .tippy-arrow:before {
+                    left: -5px;
+                    border-right-color: #252526;
+                }
+                
+                .theme__dark .tippy-box[data-theme~="wolfhead"] {
+                    color: #bdbdbd;
+                    background-color: #212121;
+                }
+                .theme__light .tippy-box[data-theme~="wolfhead"] {
+                    color: #aaa;
+                    background-color: #eee;
+                }
+                .theme__hacker .tippy-box[data-theme~="wolfhead"] {
+                    color: #bdbdbd;
+                    background-color: #252526;
+                }
+                .theme__solarized .tippy-box[data-theme~="wolfhead"] {
+                    color: #aaa;
+                    background-color: #fbf1d1;
+                }
+                .theme__kimbie .tippy-box[data-theme~="wolfhead"] {
+                    color: #aaa;
+                    background-color: #362712;
+                }
+                
+                .theme__dark .tippy-box[data-theme~="wolfhead"][data-placement~="top"] .tippy-arrow:before {
+                    border-top-color: #212121;
+                }
+                .theme__light .tippy-box[data-theme~="wolfhead"][data-placement~="top"] .tippy-arrow:before {
+                    border-top-color: #eee;
+                }
+                .theme__hacker .tippy-box[data-theme~="wolfhead"][data-placement~="top"] .tippy-arrow:before {
+                    border-top-color: #252526;
+                }
+                .theme__solarized .tippy-box[data-theme~="wolfhead"][data-placement~="top"] .tippy-arrow:before {
+                    border-top-color: #fbf1d1;
+                }
+                .theme__kimbie .tippy-box[data-theme~="wolfhead"][data-placement~="top"] .tippy-arrow:before {
+                    border-top-color: #362712;
+                }
+                
+                .theme__dark .tippy-box[data-theme~="wolfhead"][data-placement~="bottom"] .tippy-arrow:before {
+                    border-bottom-color: #212121;
+                }
+                .theme__light .tippy-box[data-theme~="wolfhead"][data-placement~="bottom"] .tippy-arrow:before {
+                    border-bottom-color: #eee;
+                }
+                .theme__hacker .tippy-box[data-theme~="wolfhead"][data-placement~="bottom"] .tippy-arrow:before {
+                    border-bottom-color: #252526;
+                }
+                .theme__solarized .tippy-box[data-theme~="wolfhead"][data-placement~="bottom"] .tippy-arrow:before {
+                    border-bottom-color: #fbf1d1;
+                }
+                .theme__kimbie .tippy-box[data-theme~="wolfhead"][data-placement~="bottom"] .tippy-arrow:before {
+                    border-bottom-color: #362712;
+                }
+                
+                .theme__dark .tippy-box[data-theme~="wolfhead"][data-placement~="left"] .tippy-arrow:before {
+                    border-left-color: #212121;
+                }
+                .theme__light .tippy-box[data-theme~="wolfhead"][data-placement~="left"] .tippy-arrow:before {
+                    border-left-color: #eee;
+                }
+                .theme__hacker .tippy-box[data-theme~="wolfhead"][data-placement~="left"] .tippy-arrow:before {
+                    border-left-color: #252526;
+                }
+                .theme__solarized .tippy-box[data-theme~="wolfhead"][data-placement~="left"] .tippy-arrow:before {
+                    border-left-color: #fbf1d1;
+                }
+                .theme__kimbie .tippy-box[data-theme~="wolfhead"][data-placement~="left"] .tippy-arrow:before {
+                    border-left-color: #362712;
+                }
+                
+                .theme__dark .tippy-box[data-theme~="wolfhead"][data-placement~="right"] .tippy-arrow:before {
+                    border-right-color: #212121;
+                }
+                .theme__light .tippy-box[data-theme~="wolfhead"][data-placement~="right"] .tippy-arrow:before {
+                    border-right-color: #eee;
+                }
+                .theme__hacker .tippy-box[data-theme~="wolfhead"][data-placement~="right"] .tippy-arrow:before {
+                    border-right-color: #252526;
+                }
+                .theme__solarized .tippy-box[data-theme~="wolfhead"][data-placement~="right"] .tippy-arrow:before {
+                    border-right-color: #fbf1d1;
+                }
+                .theme__kimbie .tippy-box[data-theme~="wolfhead"][data-placement~="right"] .tippy-arrow:before {
+                    border-right-color: #362712;
                 }
             ` }} />
         </>
