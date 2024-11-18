301ff326b04ea66b3c699342661cf28e140de6b7
davidmc.io <hi@davidmc.io>
2024-11-18 13:42:54 -0600

    Init

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,10 @@ import react from "@astrojs/react";
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: cloudflare(),
+  adapter: vercel(),
   integrations: [react(), tailwind()],
   site: 'https://crossrambles.com'
 });
