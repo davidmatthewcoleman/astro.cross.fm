b10e17d8c78d54fc68bd9220c8e2ee6d9277af6d
davidmc.io <hi@davidmc.io>
2024-12-06 16:44:05 -0600

    Init

---
+++ src/components/AboutSelector.jsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+
+export default function AboutSelector() {
+    const [currentPage, setCurrentPage] = useState('/about');
+    const [currentWidth, setCurrentWidth] = useState(0);
+    const [initialized, setInitialized] = useState(false);
+
+    useEffect(() => {
+        const page = window.location.pathname.split('/').slice(-1).pop();
+        setCurrentPage(`/about/${page === 'about' ? '' : page}`);
+        setInitialized(true);
+    }, [])
+
+    useEffect(() => {
+        // Create the function to update the width
+        const updateWidth = () => {
+            const select = document.getElementById("select-page");
+            const o = select.options[select.selectedIndex];
+            const s = document.createElement('span');
+
+            s.textContent = o.textContent;
+
+            const ostyles = getComputedStyle(select);
+            s.style.fontFamily = ostyles.fontFamily;
+            s.style.fontStyle = ostyles.fontStyle;
+            s.style.fontWeight = ostyles.fontWeight;
+            s.style.fontSize = ostyles.fontSize;
+
+
+            document.body.appendChild(s);
+
+            setCurrentWidth(s.offsetWidth + 0);
+
+
+            document.body.removeChild(s);
+        };
+
+        // Use setTimeout to delay the execution of the width calculation
+        const timeoutId = setTimeout(updateWidth, 0);
+
+        // Return a cleanup function to clear the timeout when the component is unmounted or the effect reruns
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, [currentPage]); // Dependency on currentCollection ensures it runs whenever it changes
+
+    const handleChange = (event) => {
+        const value = event.target.value;
+        window.location.href = value;
+    };
+
+    return (
+        <select id="select-page" value={currentPage} onChange={handleChange}
+                style={{'--width': `${currentWidth}px`}}>
+            <option key={0} value={'/about'}>
+                About Me
+            </option>
+            <option key={1} value={'/about/bookmarks'}>
+                Bookmarks
+            </option>
+            <option key={2} value={'/about/bookshelf'}>
+                Bookshelf
+            </option>
+            <option key={3} value={'/about/uses'}>
+                Uses
+            </option>
+        </select>
+    );
+}
